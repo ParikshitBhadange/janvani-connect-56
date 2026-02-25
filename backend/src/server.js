7af6f9d32f5f82e2df3552a14c2117f5658deb5b
Parikshit_Bhadange <parikshitbhadange00@gmail.com>
2026-02-25 16:31:37 +0530

    ready

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World!' });
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log("Server is running on port:", ENV.PORT);
+        });
+    } catch (error) {
+        console.error('Failed to start server ❌:', error.message);
+    }
+};
+
+startServer();
